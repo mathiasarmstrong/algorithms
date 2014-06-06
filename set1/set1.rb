2aaa77d60a3c4bdbe64915f685816702f1a1ab97
Jered McCullough <mathias.armstrong@gmail.com>
2014-06-06 14:05:32 -0700

    I added the new solutions to the set1.rb and commented what thier big O would be

--- set1/set1.rb
+++ set1/set1.rb
@@ -1,10 +1,34 @@
 module Set1
+  #this will have a O(n), or order n, becausewe have an iterator
   def self.swap_small(array)
+     min=array[0]
+     ind=0
+     array.each_with_index do |arg, index |
+      if arg<min
+        min=arg
+        ind=index
+      end
+    end
+     array[ind]=array[0]
+     array[0]=min
+     return array
   end
-
+#this will have a O(n^2), or order n^2, because we have an iterator within an iterator
   def self.find_sum_2(array, sum = 0)
+    a=false
+    array.each do|num1|
+      array.each{|num2| num1+num2==0 ? a=true : nil}
+    end
+    return a
   end
-
+#this will have a O(n^3), or order n^3, because we have an iterator within an iterator within an iterator
   def self.find_sum_3(array)
+    a=false
+    array.each do |num1|
+      array.each do |num3|
+        array.each{|num2| num1+num2+num3==0 ? a=true : nil}
+      end
+    end
+    return a
   end
 end
