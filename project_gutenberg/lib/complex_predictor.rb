fad75e885c07b3a279672300611fe998774d16ab
Jered McCullough <mathias.armstrong@gmail.com>
2014-07-15 10:34:36 -0700

    made some small initial changes to the complex predictor

--- project_gutenberg/lib/complex_predictor.rb
+++ project_gutenberg/lib/complex_predictor.rb
@@ -7,6 +7,21 @@ class ComplexPredictor < Predictor
   # Returns nothing.
   def train!
     @data = {}
+    all_words = Hash.new(0)
+    categories = {}
+
+
+
+    @all_books.each do |category, book|
+      book.each do |word|
+        all_words[word]+=1
+        category_words[category][word]+=1
+        categories[category]=Hash.new(0)
+      end
+    end
+
+
+
   end
 
   # Public: Predicts category.
